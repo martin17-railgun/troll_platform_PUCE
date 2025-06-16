2d0596e67d6211cb298a11ee7dadcb8a5a0ecb73
martin17-railgun <martinttb871@gmail.com>
2025-06-16 17:51:03 -0500

    hoy puce comentario

--- src/character/player/player.py
+++ src/character/player/player.py
@@ -1,11 +1,19 @@
 import pygame
+## este es mi codigo de player que le hice hace unos dias adaptale con lo que tienes para yo ver los cambios
+
+
+# como subir los cambios
+
+# en tu caso hay dos formas cuando editas un documento con un manejador de versiones en este caso
+#github te sale estas lineas
+# <- la verde es contenido nuevo, azul contenido modificado mmm espera mejor te llamo sjjsjs
 
 
 class Player:
   def __init__(self, name: str, level: int = 1):
     self.name = name
     self.level = level
-    self.health = 100
+    self.health = 150
     self.mana = 50
     self.experience = 0
 
