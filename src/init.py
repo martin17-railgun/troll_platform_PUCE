35fd64b67ab34115477b51d705285a6b6c28b772
martin17-railgun <martinttb871@gmail.com>
2025-06-22 19:41:27 -0500

    agregando movimiento no se si estara bien y si no explicame que cosa cambiar o agregar por favor detalladamente si no me mareo jsjsjsjs

--- src/init.py
+++ src/init.py
@@ -3,18 +3,24 @@ import pygame
 from src.character.player.player import Player
 from src.const.game import SCREEN
 
-
 class Game_controller:
   running = True
   player = Player("Player")
+  sprites = pygame.sprite.Group()
+
 
   def __init__(self):
     self._game = None
+    self.sprites.add(self.player)
 
   def start(self):
     SCREEN.fill((0, 0, 0))
+    self.get_events(pygame.event)
+    # self.player.draw()
+    self.sprites.draw(SCREEN)
+    self.sprites.update()
 
-    self.player.draw()
+    print(f"Player position: ({self.player.x}, {self.player.y})")
 
   def get_events(self, event):
     for event in pygame.event.get():
